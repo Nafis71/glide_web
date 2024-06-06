127af127eedcd557c9a177492481e756e607c33f
Nafis71 <nhtonmoy2@gmail.com>
2024-06-07 03:42:01 +0600

    ios page transition

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:glide_web/views/homeScreen/home_screen.dart';
 import 'package:glide_web/views/webViewScreen/web_view_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Routes{
   static const String homeScreen ="/homeScreen";
   static const String webViewScreen ="/webViewScreen";
 
 
-  static MaterialPageRoute? generateRoute(RouteSettings routeSettings){
-    final Map<String,WidgetBuilder> routes = {
-      homeScreen : (context) => const HomeScreen(),
-      webViewScreen : (context) => const WebViewScreen(),
+  static PageTransition? generateRoute(RouteSettings routeSettings){
+    final Map<String,PageTransition> routes = {
+      homeScreen : PageTransition(
+    type: PageTransitionType.rightToLeft,
+    child: const HomeScreen(),
+    isIos: true,
+    ),
+      webViewScreen : PageTransition(
+    type: PageTransitionType.rightToLeft,
+    child: const WebViewScreen(),
+    isIos: true,
+    ),
     };
-    final WidgetBuilder? builder = routes[routeSettings.name];
-    return (builder != null) ? MaterialPageRoute(builder: builder): null;
+    final PageTransition? pageTransition = routes[routeSettings.name];
+    return (pageTransition !=null) ? pageTransition : null;
 
   }
 
